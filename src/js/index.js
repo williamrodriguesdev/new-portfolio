eed4a36af24ab8228204f35ab49c8e87d44e584b
williamrodriguesdev <williamrodrigues_dev@outlook.com>
2024-10-02 21:21:18 -0300

    fix responsividade

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,12 @@ hamburguer.addEventListener('click', () => {
   hamburguer.classList.toggle('active');
   navMenu.classList.toggle('active');
 })
+
+document.getElementById("btn-voltar").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
   
